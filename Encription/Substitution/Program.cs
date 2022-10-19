c3cefb14d4897b4d4168827ab9d0abf7acccff63
Horea Oros <horea.oros@gmail.com>
2022-10-19 09:11:54 +0300

    Add project files.

---
+++ Encription/Substitution/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substitution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShiftByNCipher cipher = new ShiftByNCipher(1);
+            cipher.PlainText = "Hello World";
+            cipher.Encrypt();
+            Console.WriteLine(cipher.CipherText);
+
+            cipher.CipherText = "IFMMP XPSME";
+            cipher.Decrypt();
+            Console.WriteLine(cipher.PlainText);
+
+            CaesarCipher c = new CaesarCipher();
+            //c.PlainText = "Caesar is here!";
+            //c.Encrypt();
+            //Console.WriteLine(c.CipherText);
+
+            c.CipherText = "FDHVDU LV KHUH!";
+            c.Decrypt();
+            Console.WriteLine(c.PlainText);
+
+            ROT13 rot13 = new ROT13();
+
+            MonoalphabeticSubstitution ms = new MonoalphabeticSubstitution();
+        }
+    }
+}
---
+++ Encription/Substitution/Util.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+namespace Substitution
+{
+    internal class Util
+    {
+        public static void Shuffle(StringBuilder sb)
+        {
+
+        }
+    }
+}
