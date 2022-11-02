92f25ef0928231b55106126c55e76fce071f39e8
Horea Oros <horea.oros@gmail.com>
2022-11-02 12:00:01 +0200

    add wpf project

---
+++ SymmetricEncyption/SymmetricEncyption/DataContext.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Security.Cryptography;
+public class Context
+{
+	public string[] CipherMode => Enum.GetNames(typeof(CipherMode)); 
+	public string[] PaddingMode => Enum.GetNames(typeof(PaddingMode));
+}
---
+++ SymmetricEncyption/SymmetricEncyption/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace SymmetricEncyption
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure open file dialog box
+            var dialog = new OpenFileDialog();
+            //dialog.FileName = "Document"; // Default file name
+            //dialog.DefaultExt = ".txt"; // Default file extension
+            //dialog.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.FileName;
+                lblFileName.Content = filename;
+            }
+        }
+    }
+}
